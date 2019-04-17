1fe70ac7a53d55c0046b1e409035b3d9f776198a
Andrea Bellotto <andreabellotto88@gmail.com>
2019-04-17 16:12:09 +0200

    test params

--- controller/EmptyBoneController.swift
+++ controller/EmptyBoneController.swift
@@ -4,6 +4,7 @@ import FluentPostgreSQL
 
 struct {{name | firstUppercase}}Controller:RouteCollection{
     func boot(router: Router) throws {
-    
+    	print("{{parent}}")
+    	print("{{children}}")
     }
 }
