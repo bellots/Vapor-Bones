e53e7f5216a03aed890c255470336d39602282d2
Andrea Bellotto <andreabellotto88@gmail.com>
2019-09-06 18:08:02 +0200

    removed useless import in model

--- model/BoneModel.swift
+++ model/BoneModel.swift
@@ -6,7 +6,7 @@ final class {{name | firstUppercase}}:Codable{
     var id:Int?
 }
 
-extension {{name | firstUppercase}}:{% if database %}{% if database == "psql"%}import PostgreSQLModel{% else %}import MySQLModel{% endif %}{% else %}import PostgreSQLModel{%endif%}{}
+extension {{name | firstUppercase}}:{% if database %}{% if database == "psql"%}PostgreSQLModel{% else %}MySQLModel{% endif %}{% else %}PostgreSQLModel{%endif%}{}
 extension {{name | firstUppercase}}:Parameter{}
 extension {{name | firstUppercase}}:Migration{}
 extension {{name | firstUppercase}}:Content{}
